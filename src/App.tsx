5073d2fb73f87df6bd4bfef12f60fdb95830517b
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-02-09 09:25:51 +0100

    Project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Socket } from "socket.io-client";
+const socket = Socket.connect('http://localhost:3000');
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Chat
     </div>
   );
 }
