bfc9da62306f64863d5109dc5ba1fa3a6f6f2461
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-02-10 16:25:36 +0100

    Drawer + groups and chats listing

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,34 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+
+//Components
+import { LeftDrawer } from "./Drawer/Drawer";
+
+import axiosInstance from "../../shared/network/axios";
 
 const Home: FC = () => {
-    return <>Home! Start the chat!</>
+
+    const [persons, setPersons] = useState<any[]>([]);
+    const [groups, setGroups] = useState<any[]>([]);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const response = await axiosInstance.get('user/info');
+            const { persons, groups } = response.data;
+            setPersons(persons);
+            setGroups(groups);
+        }
+
+        fetchData();
+
+        return () => {
+            setGroups([]);
+            setPersons([]);
+        }
+    }, [])
+    return <>
+        <LeftDrawer persons={persons} groups={groups}></LeftDrawer>
+    </>
 }
 
 export default Home;
