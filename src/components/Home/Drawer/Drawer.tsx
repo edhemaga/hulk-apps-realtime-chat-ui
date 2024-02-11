f828c06d9daa2fea7124e6dd2efa30fe1011fc03
Edim Hadžić <edimhadzic98@gmail.com>
2024-02-11 15:26:10 +0100

    Minor refactor

--- src/components/Home/Drawer/Drawer.tsx
+++ src/components/Home/Drawer/Drawer.tsx
@@ -1,17 +1,17 @@
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import { FC } from "react";
-import { Button } from "@mui/material";
+
+import {
+  Box,
+  Drawer,
+  CssBaseline,
+  Toolbar,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Button,
+} from "@mui/material";
 
 const drawerWidth = 300;
 
@@ -47,7 +47,7 @@ export const LeftDrawer: FC<Props> = ({ groups, persons, openChat }) => {
         <List>
           <div className="flex">
             <div className="m-auto font-bold">Groups</div>
-            <Button className='mt-auto mb-auto mr-1 ml-1'>Add new group</Button>
+            <Button className="mt-auto mb-auto mr-1 ml-1">Add new group</Button>
           </div>
           {groups?.map((group, index) => (
             <ListItem key={group.id} disablePadding>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useLayoutEffect, useState } from "react";
 
 import { JwtPayload, jwtDecode } from "jwt-decode";
 import axiosInstance from "../../shared/network/axios";
@@ -20,46 +20,55 @@ const Home: FC = () => {
   const [persons, setPersons] = useState<IUser[]>([]);
   const [groups, setGroups] = useState<IGroup[]>([]);
 
-  const [currentUser, setCurrentUser] = useState<string>();
+  const [currentUser, setCurrentUser] = useState<string | undefined>();
   const [selectedGroup, setSelectedGroup] = useState<IGroup>();
+
+  useLayoutEffect(() => {
+    let claims: JwtPayloadUserClaims = jwtDecode(
+      localStorage.getItem("access_token") ?? ""
+    );
+    setCurrentUser(claims.id);
+
+    return () => {
+      setCurrentUser(undefined);
+    };
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
-      let claims: JwtPayloadUserClaims = jwtDecode(
-        localStorage.getItem("access_token") ?? ""
-      );
-      setCurrentUser(claims.id);
-
-      const response = await axiosInstance.get("user/info");
+      const response = await axiosInstance.get(`user/info/${currentUser}`);
 
       const { persons, groups } = response.data;
-      const remappedPersons = persons
-        .filter((arg: IUser) => arg.id !== claims.id)
-        .map((person: IUser) => {
-          return {
-            id: person.id,
-            name: `${person.name} ${person.surname}`,
-          };
-        });
+
+      const remappedPersons = persons.map((person: IUser) => {
+        return {
+          id: person.id,
+          name: `${person.name} ${person.surname}`,
+        };
+      });
 
       setPersons(remappedPersons);
       setGroups(groups);
     };
 
-    fetchData();
+    if (currentUser) fetchData();
 
     return () => {
       setGroups([]);
       setPersons([]);
     };
-  }, []);
+  }, [currentUser]);
 
   const openChat = async (id: string, type: string) => {
-    const response = await axiosInstance.get<IGroup>(
-      `/group/${currentUser}/${id}`
-    );
+    try {
+      const response = await axiosInstance.get<IGroup>(
+        `/group/${currentUser}/${id}`
+      );
 
-    setSelectedGroup(response.data as IGroup);
+      setSelectedGroup(response.data as IGroup);
+    } catch (e) {
+      //TODO Add toaster with error message
+    }
   };
 
   return (
